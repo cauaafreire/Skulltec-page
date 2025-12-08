ad361588752ed1cc4c06c4733526b2f776e36ba9
Cauã <cauafreirw@gmail.com>
2025-12-08 11:09:19 -0300

    arrumando a logica do cadastrar

--- src/php/cadastro.php
+++ src/php/cadastro.php
@@ -6,7 +6,7 @@
   $vsexo=$_POST['txtsexo'];
   $vformacao=$_POST['txtformacao'];
   $vexperiencia=$_POST['txtexperiencia'];
-  $cadastrar = $cmd->query("insert into tbCurriculo(cpf_t,nome_t,emai_t,sexo_t,experiencia_t,formacao_t) values ('$vcpf','$vnome','$vemai','$vsexo','$vformacao','$vexperiencia')");
+  $cadastrar = $cmd->query("insert into tbCurriculo(cpf_t,nome_t,emai_t,sexo_t,experiencia_t,formacao_t) values ('$vcpf','$vnome','$vemai','$vsexo','$vexperiencia','$vformacao')");
   echo "<script language='JavaScript'>
     alert('Curriculo cadastrado com sucesso!!!');
     location.href='cadastro.html';
